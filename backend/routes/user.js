ba78f9343399765858739fafb528b3504bb00e2e
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-08-06 03:00:56 +0530

    added category-routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { getUserByID, getUser, updateUser } = require('../controllers/user');
 const { isSignedIN, isAuthenticated, isAdmin } = require('../controllers/auth');
 
-router.param('userID', getUserByID); // Get User-ID from URL
+router.param('userID', getUserByID); // Fetch User-ID from URL
 
 router.post('/user/:userID',isSignedIN, isAuthenticated, getUser); // Route for Getting User NOTE:- Only if User if SignedIN and is Authenticated will be able to get User
 router.put('/user/:userID',isSignedIN, isAuthenticated, updateUser); // Route for Updating User NOTE:- Only If User is SignedIn and is Authenticated will be able to update User
