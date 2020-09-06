097db42967c0477fad9c270c090a2cf575e3da89
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-09-06 06:47:14 +0530

    added changes to producr route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -11,8 +11,10 @@ router.post('/product/create/:userID', isSignedIN, isAuthenticated, isAdmin, cre
 router.get('/product/:productID', getProduct); // get single product
 router.get('/product/photo/:productID', getPhoto); // get product photo
 router.get('/products', getProducts); // get all products
+
 router.put('/product/:productID/:userID', isSignedIN, isAuthenticated, isAdmin, updateProduct) // update product
 router.delete('/product/:productID/:userID', isSignedIN, isAuthenticated, isAdmin, deleteProduct) // delete product
+
 router.get('/products/categories', getAllUniqueCategories); // get all unique categories
 
 module.exports = router;
